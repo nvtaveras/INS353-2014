cadc26981040c916c22574fa1b3469df07377ae6
Nelson Taveras <ntaveras03@gmail.com>
2014-05-29 23:06:47 -0400

    Implemented Product class

--- class03/lib/product.rb
+++ class03/lib/product.rb
@@ -1,3 +1,11 @@
 class Product
-  
+
+  attr_accessor :title, :price, :category
+
+  def initialize title, price, category
+    @title = title
+    @price = price
+    @category = category
+  end
+
 end
