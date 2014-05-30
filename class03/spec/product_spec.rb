428883f9b3b7c3abef44d59157de0c46475da655
Nelson Taveras <ntaveras03@gmail.com>
2014-05-29 23:16:04 -0400

    Implemented initialize and title tests

--- class03/spec/product_spec.rb
+++ class03/spec/product_spec.rb
@@ -3,18 +3,21 @@ require 'spec_helper.rb'
 describe Product do
   
   before :each do
-
+    @P = Product.new('Macbook Pro', 1599.99, :electronics)
   end
 
   describe "When creating a new product" do
     context "With valid parameters" do
-      it "should take 3 parameters and return a product object"
+      it "should take 3 parameters and return a Product object" do
+        expect @P.class.should == Product
+      end
     end
   end
 
   describe "#title" do
-    it "should return the product title"
-
+    it "should return the product title" do
+      expect @P.title.should == 'Macbook Pro'
+    end
     it "should be a string"
   end
 
