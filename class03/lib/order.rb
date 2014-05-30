d7e840dbf00a671b6a80b6ef5a762878b49daefe
Nelson Taveras <ntaveras03@gmail.com>
2014-05-29 23:23:39 -0400

    Added Order class methods

--- class03/lib/order.rb
+++ class03/lib/order.rb
@@ -1,3 +1,23 @@
 class Order
-    
+  
+  attr_accessor :products
+
+  def initialize
+  end
+
+  def filter_by_category
+  end
+
+  def filter_by_price
+  end
+
+  def add_product
+  end
+
+  def get_product
+  end
+
+  def save
+  end
+
 end
