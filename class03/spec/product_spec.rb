d76e668f443aea647923bfb385b704b6d557df7c
Nelson Taveras <ntaveras03@gmail.com>
2014-05-29 23:11:17 -0400

    Added test examples for the Product class

--- class03/spec/product_spec.rb
+++ class03/spec/product_spec.rb
@@ -2,4 +2,32 @@ require 'spec_helper.rb'
 
 describe Product do
   
+  before :each do
+
+  end
+
+  describe "When creating a new product" do
+    context "With valid parameters" do
+      it "should take 3 parameters and return a product object"
+    end
+  end
+
+  describe "#title" do
+    it "should return the product title"
+
+    it "should be a string"
+  end
+
+  describe "#price" do
+    it "should return the product price"
+
+    it "should be a float"
+  end
+
+  describe "#category" do
+    it "should return the product category"
+
+    it "should be a symbol"
+  end
+
 end
