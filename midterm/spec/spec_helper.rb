7bb9ba4af73a0677d95e1ae132033e7110421403
Nelson Taveras <ntaveras03@gmail.com>
2014-06-05 10:09:57 -0400

    Added spec_helper form Josh's repo

--- midterm/spec/spec_helper.rb
+++ midterm/spec/spec_helper.rb
@@ -1,10 +1,10 @@
-# ["order", "product"].each do |f|
-#   require File.join(File.dirname(__FILE__), '..', "lib/#{f}.rb")
-# end
+["post", "user"].each do |f|
+    require File.join(File.dirname(__FILE__), '..', "lib/#{f}.rb")
+end
 
 require 'yaml'
 
 RSpec.configure do |config|
   config.color_enabled = true
-  config.formatter = :progress
+  config.formatter = :documentation
 end
