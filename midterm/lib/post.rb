752cb5aaac9a8dcce2f3f0a472abf9c59e81ebf8
Nelson Taveras <ntaveras03@gmail.com>
2014-06-05 10:18:33 -0400

    Implemented initialize method

--- midterm/lib/post.rb
+++ midterm/lib/post.rb
@@ -1,3 +1,15 @@
 class Post
 
+  attr_accessor :title, :text, :date, :user
+
+  def initialize *args
+    if args.length == 1
+      # load from YAML file
+    else
+      @title = args[0]
+      @text = args[1]
+      @date = args[2]
+      @user = args[3]
+  end
+
 end
