ca45b1a114501df1581a949058fd24a93be3e118
Nelson Taveras <ntaveras03@gmail.com>
2014-06-05 10:43:01 -0400

    Implemented tests for posts method

--- midterm/spec/user_spec.rb
+++ midterm/spec/user_spec.rb
@@ -5,7 +5,8 @@ describe User do
   before :each do
     @user = User.new('ntaveras')
     @user.add_post('My Awesome Post', 'First Post!', Date.new(2014, 05, 06))
-    @user.add_post('My Second Awesome Post', 'Second Post!', Date.new(2014, 05, 06))
+    @user.add_post('My Second Awesome Post', 'Second Post!', Date.new(2014, 05, 16))
+    @user.add_post('My Third Awesome Post', 'Third Post!', Date.new(2014, 05, 03))
   end
 
   describe "#initialize" do
@@ -16,7 +17,16 @@ describe User do
 
   describe "#add_post" do
     it "should add a post to the posts list" do
-      expect @user.posts_list.length.should == 2
+      expect @user.posts_list.length.should == 3
+    end
+  end
+
+  describe "#posts" do
+    it "should return the posts added in reverse chronological order" do
+      post = @user.posts
+      expect post[0].date.should == Date.new(2014, 05, 16)
+      expect post[1].date.should == Date.new(2014, 05, 06)
+      expect post[2].date.should == Date.new(2014, 05, 03)
     end
   end
 
